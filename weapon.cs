c9be7d801468d1f0386e651f823775970b4b2dd7
asher <aw0534145493@gmail.com>
2025-05-27 10:29:00 +0300

    skeleton of weapon class

---
+++ weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    public class Weapon
+    {
+        public string name;
+        public string manufacturer;
+        public int numberOfBalls;
+
+        public Weapon (string Name, string Manufacturer, int NumberOfBalls)
+        {
+            name = Name;
+            manufacturer = Manufacturer;
+            numberOfBalls = NumberOfBalls;
+        }
+
+        public void Shoot()
+        {
+
+        }
+
+    }
+}
